562e39963c3c5eea6719cde406b5994661c4bce5
Marco Minotto <marcoaminotto@hotmail.com>
2018-03-22 18:21:39 -0300

    Adicionado a entrada de informacao do usuario para o teste

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,5 +1,14 @@
+
 puts "Bem-vindo ao jogo da adivinhação!"
 puts "Qual o seu nome?"
-gets nome
+nome = gets
 
-puts "Começamos o jogo para voce, #{nome}"
+puts "Começamos o jogo para voce, #{nome}."
+puts "Escolhendo um  numero secreto entre 0..200."
+valor = rand 0..200
+
+puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+puts "Entre com um numero."
+tentativa = gets
+
+puts "Sera que você acertou? Voce chutou #{tentativa}"
